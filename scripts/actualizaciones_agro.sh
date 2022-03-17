af6937a68c53c4a00cf0e7b23f6af17a95bab1c5
VivianBass <vivbass4@gmail.com>
2022-03-17 16:56:56 -0600

    update script

--- scripts/actualizaciones_agro.sh
+++ scripts/actualizaciones_agro.sh
@@ -1,17 +1,28 @@
 #!/bin/sh
-exec >logfile.txt 2>&1
+exec >scripts/logfile.txt 2>&1
+
+TODAY=`date`
+echo $TODAY
+
 echo "Empieza validación de actualizaciones diarias\n"
 echo "Cambiando nombre de archivos..."
 mv agro_actual.csv agro_anterior.csv 
 echo "Descargando archivo nuevo..." 
 #SE LE TIENE QUE QUITAR EL -it
 docker exec etiqueta-postgres psql -U postgres -d zendro_development -c "COPY agrobiodiversidads TO STDOUT WITH CSV HEADER" > agro_actual.csv
-echo "Comparando archivos..."
+#echo "Comparando archivos..."
 /usr/bin/python3 compare.py
-echo "git add history.txt"
-/usr/bin/git add history.txt
+cp /home/vbass/etiquetas/etiquetas/graphql-server/history.txt ./history.md
+cp /home/vbass/etiquetas/etiquetas/graphql-server/changelog.txt ./changelog.md
+echo "git add history.md changelog.md"
+/usr/bin/git add history.md changelog.md
 echo "git commit"
 /usr/bin/git commit -m "validacion diaria de registros"
 echo "git push"
 /usr/bin/git push 
 echo "Proceso terminado!"
+/usr/bin/git add scripts/logfile.txt
+echo "git commit"
+/usr/bin/git commit -m "validacion diaria de registros"
+echo "git push"
+/usr/bin/git push 
