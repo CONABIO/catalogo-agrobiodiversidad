255178595e6a07dd135927bbeee4502408b9585f
VivianBass <vivbass4@gmail.com>
2022-10-21 15:26:09 -0500

    send mail in check_pendiente

--- scripts/check_pendiente.py
+++ scripts/check_pendiente.py
@@ -59,14 +59,17 @@ def get_pendientes():
     r = session.post(path_siagro, json={'query': query}, verify=False)
     json_data = json.loads(r.text)
     json_data = json_data['data']['agrobiodiversidads']
+    actual=pd.read_csv(path_actual, keep_default_na=False)   
+    anterior=pd.read_csv(path_anterior, keep_default_na=False)   
     #print(json_data['data']['agrobiodiversidads'])
     aux=""
-    # for i in range (len(json_data)):
-    #     #print(json_data[i]['taxon'])
-    #     res=search_taxon(json_data[i])
-    #     aux=aux + res 
+    for i in range (len(json_data)):
+        #print(json_data[i]['taxon'])
+        res=search_taxon(json_data[i],actual,anterior)
+        aux=aux + res 
 
-    sendeMail(aux)    
+    if aux != "":
+        sendeMail(aux)    
 
 
 def is_equal(info,json_data):
@@ -93,8 +96,15 @@ def sendeMail(string):
     remitente = "SIAgro <siagro@siagro.conabio.gob.mx>" 
     #destinatario = ["Vivian <vivbass4@gmail.com>", "Vivian <vbass@conabio.gob.mx>", "Alicia <amastretta@conabio.gob.mx>", "Irene <iramos@conabio.gob.mx>"]
     destinatario = ["Vivian <vbass@conabio.gob.mx>"]
-    asunto = "Revisar pendiente" 
-    mensaje = """hola"""
+    asunto = "Revisar taxones con id pendiente" 
+    mensaje = """La siguiente lista de taxones pendientes tiene similitudes con los taxones validos indicados, favor de revisar los campos categoria_agrobiodiversidad, es_parientesilvestre, es_domesticado y es_quelite.
+
+ ID pendiente | Taxon valido
+
+"""+string+"""
+    
+------------------------------------
+Este correo no contiene acentos y ha sido enviado automaticamente. Favor de no responder."""
     email = 'Subject: {}\n\n{}'.format(asunto, mensaje)
     try: 
         smtp = smtplib.SMTP('localhost') 
@@ -105,7 +115,13 @@ def sendeMail(string):
         Compruebe que el mensaje no tenga acentos""")
 
 
-def search_taxon(info):
+def is_new(id,actual,anterior):
+    if (actual['id'].isin([id]).any()) and (anterior['id'].isin([id]).any() == False):
+        return True
+    else:
+        return False
+
+def search_taxon(info,actual,anterior):
     #print("taxon:", taxon)
     taxon_split=info['taxon'].split(' ')
     #print("taxon array:", taxon)
@@ -130,20 +146,18 @@ def search_taxon(info):
     r = session.post(path_siagro, json={'query': query}, verify=False)
     json_data = json.loads(r.text)
     json_data = json_data['data']['agrobiodiversidads']
-
+    formail=""
     if len(json_data) > 0 :
         for i in range(len(json_data)):
-            formail=""
+            
             #print(json_data[0]['taxon'])
             if is_equal(info,json_data[i]):
                 delete_pendiente(info['id'])
 
             else:
-                print("\n*************")
-                print("Hay algo diferente")
-                formail="hola"
-    else:
-        formail=""
+                if is_new(json_data[i]['id'],actual,anterior):
+                    formail=formail + info['id'] + " | " + json_data[i]['id'] +"\n" 
+
     return formail            
 
 if __name__ == '__main__':
